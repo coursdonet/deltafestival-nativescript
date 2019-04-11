05aea71fbb3718ff5a06efa160e40c7508589dc1
Tom Ruscelli <tom.ruscelli@ynov.com>
2019-04-11 14:54:21 +0200

    commencement ranking

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import * as app from "tns-core-modules/application";
+import { RankingService } from "../api/services";
 
 @Component({
   selector: 'ns-ranking',
@@ -9,15 +10,21 @@ import * as app from "tns-core-modules/application";
   moduleId: module.id,
 })
 export class RankingComponent implements OnInit {
+  public ranking: any;
 
-  constructor() { }
+  constructor(private rankingService: RankingService) { }
 
   ngOnInit() {
+    this.rankingService.Ranking().subscribe((ranking) => {
+      console.log("ok", ranking);
+    });
   }
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
   }
+
+
 
 }
